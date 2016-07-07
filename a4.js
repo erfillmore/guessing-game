8f80727276e7501da3a625bd3613473a1e80a9f1
Elisabeth Fillmore <Elisabeth Fillmore>
2016-07-06 19:18:23 -0700

    first commit

---
+++ a4.js
@@ -0,0 +1,58 @@
+var preferredName == null;
+function biggestFunction() {
+if (preferredName == null) {
+  preferredName = getName;
+}
+  function getName() {
+    while (preferredName == null || preferredName == "") {
+    preferredName = prompt("Hello Friend! What's your prefered name?");
+  }
+    console.log(preferredName);
+  }
+    alert("Welcome, " + preferredName + "!");
+
+//     var guessPet = prompt("How many pets do I have?", "");
+//     if (guessPet == 2) {
+//         alert("Yay, you paid attention!");
+//     } else if (guessPet > 2) {
+//         alert("Your number is too high! I have two pets!");
+//     } else {
+//         alert("Your guess is too low! I have two pets!");
+//     }
+//
+//     console.log("Presenting question 2.");
+//     var baGuess = "";
+//     while (baGuess === "") {
+//         baGuess = prompt("What subject did I major in at university?", "");
+//         if (baGuess.toLowerCase() == "english literature" || baGuess.toLowerCase() == "english" || baGuess.toLowerCase() == "literature") {
+//             alert("Well Done!");
+//         } else {
+//             alert(":/ I studied English Literature....");
+//         }
+//     }
+//     console.log("Question III!");
+//     var goalGuess = prompt("What is my life goal?");
+//     if (goalGuess.toLowerCase() == "have my pets living in luxury" || baGuess.toLowerCase() == "pets in luxury" || baGuess.toLowerCase() == "luxurious life for pets") {
+//         alert("Woop!");
+//     } else {
+//         alert("No! I want my pets to be able to live in the lap of luxury");
+//     }
+//     console.log("Last question");
+//
+//
+//     var count = prompt("How many alert boxes do you think I can create?", "");
+//     if (isNaN(count)) {
+//         count = prompt("That is not a number, silly! Try again!", "");
+//     }
+//     if (count == 0) {
+//         alert("Well, don't worry, I didn't want to play your game ANYWAY");
+//     } else if (count => 3) {
+//         alert("Really?! " + count + " is way too much. I refuse to do this. I am not your puppet");
+//     } else {
+//         alert("Fiiiiiine.")
+//     }
+//
+//     for (count; count < 0; count++) {
+//         alert("Count = " + count);
+//     }
+}
