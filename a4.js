7b1fcab7b9fcd338c98dc7315335a2cfc04022a8
Elisabeth Fillmore <Elisabeth Fillmore>
2016-07-10 17:11:18 -0700

    assignment number 5

--- a4.js
+++ a4.js
@@ -1,4 +1,5 @@
 var preferredName;
+var score = 0
 
 function getName() {
     while (preferredName == null || preferredName == "") {
@@ -11,51 +12,55 @@ function getName() {
 function firstQuestion() {
     var guessPet = prompt("How many pets do I have?", "");
     if (guessPet == 2) {
-        document.getElementById("firstQ").innerHTML="Your answer of " +guessPet+ " was correct!";
+        document.getElementById("firstQ").innerHTML = "Your answer of " + guessPet + " was correct!";
+        var colorfirstQ = document.getElementById("firstQ");
+        colorfirstQ.className = "correctColor";
+        score++;
     } else if (guessPet > 2) {
-        document.getElementById('firstQ').innerHTML="Your answer of " +guessPet+ " was too high, and thus incorrect!";
+        document.getElementById('firstQ').innerHTML = "Your answer of " + guessPet + " was too high, and thus incorrect!";
+        var colorfirstQ = document.getElementById("firstQ");
+        colorfirstQ.className = "inCorrectColor"
     } else {
-        document.getElementById("firstQ").innerHTML="Your answer of " +guessPet+ " was too low, and thus incorrect!";
+        document.getElementById("firstQ").innerHTML = "Your answer of " + guessPet + " was too low, and thus incorrect!";
+        var colorfirstQ = document.getElementById("firstQ");
+        colorfirstQ.className = "inCorrectColor"
     }
 }
 
-function secondQuestion() {
-  var baGuess = "";
-  while (baGuess === "") {
-      baGuess = prompt("What subject did I major in at university?", "");
-      if (baGuess.toLowerCase() == "english literature" || baGuess.toLowerCase() == "english" || baGuess.toLowerCase() == "literature") {
-          document.getElementById("secondQ").innerHTML="Your answer of " +baGuess+ " was correct!";
-      } else {
-          document.getElementById("secondQ").innerHTML="Your answer of " +baGuess+ " was incorrect!";
-      }
-  }
-}
-
-function thirdQuestion() {
-      var goalGuess = prompt("What is my life goal?");
-      if (goalGuess.toLowerCase() == "have my pets living in luxury" || goalGuess.toLowerCase() == "pets in luxury" || goalGuess.toLowerCase() == "luxurious life for pets") {
-        document.getElementById("thirdQ").innerHTML="Your answer of " +goalGuess+ " was correct!";
-      } else {
-          document.getElementById("thirdQ").innerHTML="Your answer of " +goalGuess+ " was incorrect!";
-      }
+function secondthirdQuestion(promptAnswer, rightAnswer, id) {
+    var questionGuess = prompt(promptAnswer)
+    if (questionGuess.toLowerCase() == rightAnswer) {
+        document.getElementById(id).innerHTML = "Your answer of " + questionGuess + " was correct!";
+        var colorfirstQ = document.getElementById(id);
+        colorfirstQ.className = "correctColor";
+        score++;
+    } else {
+        document.getElementById(id).innerHTML = "Your answer of " + questionGuess + " was incorrect!";
+        var colorfirstQ = document.getElementById(id);
+        colorfirstQ.className = "inCorrectColor"
+    }
 }
 
 function fourthQuestion() {
-  var count = prompt("How many alert boxes do you think I can create?", "");
-  if (isNaN(count)) {
-      count = prompt("That is not a number, silly! Try again!", "");
-  }
-  if (count == 0) {
-    document.getElementById("fourthQ").innerHTML="Well, don't worry, I didn't want to play your game ANYWAY";
-  } else if (count => 3) {
-      document.getElementById("fourthQ").innerHTML="Really?! " + count + " is way too much. I refuse to do this. I am not your puppet";
-  } else {
-      document.getElementById("fourthQ").innerHTML="Fiiiiiine."
-  }
+    var count = prompt("How many alert boxes do you think I can create?", "");
+    if (isNaN(count)) {
+        count = prompt("That is not a number, silly! Try again!", "");
+    }
+    if (count == 0) {
+        document.getElementById("fourthQ").innerHTML = "Well, don't worry, I didn't want to play your game ANYWAY";
+        var colorfirstQ = document.getElementById("fourthQ");
+        colorfirstQ.className = "inCorrectColor"
+        score++
+    } else if (count => 3) {
+        document.getElementById("fourthQ").innerHTML = "Really?! " + count + " is way too much. I refuse to do this. I am not your puppet";
+        var colorfirstQ = document.getElementById("fourthQ");
+        colorfirstQ.className = "inCorrectColor"
+    } else {
+        document.getElementById("fourthQ").innerHTML = "Fiiiiiine."
+        var colorfirstQ = document.getElementById("fourthQ");
+        colorfirstQ.className = "inCorrectColor"
+    }
 
-  for (count; count < 0; count++) {
-      alert("Count = " + count);
-  }
 }
 
 function biggestFunction() {
@@ -69,11 +74,27 @@ function biggestFunction() {
         getName();
     }
 
-    secondQuestion();
+    secondthirdQuestion("What subject did I major in at university?", "english literature", "secondQ");
 
-    thirdQuestion();
+    secondthirdQuestion("What is my life goal?", "have my pets living in luxury", "thirdQ");
 
     fourthQuestion();
 
+    document.getElementById("score").innerHTML = "You got " + score + " right!"
+    if (score >= 2) {
+        var win = document.createElement("IMG");
+        win.setAttribute("src", "codefellows1.jpg");
+        win.setAttribute("width", "304");
+        win.setAttribute("width", "228");
+        win.setAttribute("alt", "Serena Williams");
+        document.body.appendChild(win);
+    } else {
+        var lose = document.createElement("IMG");
+        lose.setAttribute("src", "codefellows2.jpg");
+        lose.setAttribute("width", "304");
+        lose.setAttribute("width", "228");
+        lose.setAttribute("alt", "You lost!");
+        document.body.appendChild(lose);
+    }
 
 }
